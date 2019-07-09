cbb33ee83db2cd2e5d6a15a67311d791de0039a8
ffraniel <francis.whitehead@googlemail.com>
2019-07-09 15:36:00 +0100

    created initial files

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const Home = ()  => (
+  <section className="home">
+    <h1>Fran The Web Dev</h1>
+    <style jsx>{`
+    a {
+      text-decoration: none;
+    }
+  `}</style>
+  </section>
+);
+
+
+export default Home;
---
+++ pages/process.js
@@ -0,0 +1,5 @@
+function Process () {
+  return <div>processes</div>;
+}
+
+export default Process;
