ea8a739c13620ce32cc1174d7d12c5c3f4e4c723
ffraniel <francis.whitehead@googlemail.com>
2019-07-11 15:15:46 +0100

    footer links content

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,45 @@
+import Link from 'next/link';
+
 const Footer = () => (
-  <footer className="Footer">
-    <h2>Footer</h2>
+  <footer className="footer">
+    <Link 
+      href={"/"} 
+      as={"/"}>
+      <a>Home</a>
+    </Link>
+    <Link 
+      href={"/products"} 
+      as={"/products"}>
+      <a>Products</a>
+    </Link>
+    <Link 
+      href={"/process"} 
+      as={"/process"}>
+      <a>Process</a>
+    </Link>
+    <Link 
+      href={"/faq"} 
+      as={"/faq"}>
+      <a>FAQ</a>
+    </Link>
     <style jsx>{`
-      .Footer {
+      .footer {
         width: 100%;
         background-color: #bfa3bf;
-        min-height: 80px;
+        height: 80px;
+        display: flex;
       }
-      h2 {
+      .footer a {
         color: white;
+        padding: 10px 30px;
+        text-decoration: none;
+        text-align: center;
+
+        transition: background-color 0.25s;
         margin: var(--header-footer-margin);
+      }
+      .footer a:hover {
+        background: var(--link-hover-bck-main);
       }
     `}</style>
   </footer>
