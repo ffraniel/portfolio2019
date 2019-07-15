5aab1ab916bc681ea742757bdff91acc2b557951
ffraniel <francis.whitehead@googlemail.com>
2019-07-15 18:22:45 +0100

    addded sections and icons

--- components/Footer.js
+++ components/Footer.js
@@ -2,51 +2,52 @@ import Link from 'next/link';
 
 const Footer = () => (
   <footer className="footer">
-    <Link 
-      href={"/"} 
-      as={"/"}>
-      <a>Home</a>
-    </Link>
-    <Link 
-      href={"/products"} 
-      as={"/products"}>
-      <a>Products</a>
-    </Link>
-    <Link 
-      href={"/process"} 
-      as={"/process"}>
-      <a>Process</a>
-    </Link>
-    <Link 
-      href={"/faq"} 
-      as={"/faq"}>
-      <a>FAQ</a>
-    </Link>
-    <h5 className="phone">Phone: 08771 746 863</h5>
+    
+    <section className="icon-list">
+      <a href="facebook.com">
+        <img src="static/facebook48.png" alt="Facebook" />
+      </a>
+      <a href="twitter.com">
+        <img src="static/twitter48.png" alt="Twitter" />
+      </a>
+      <a href="github.com">
+        <img src="static/github48.png" alt="Github" />
+      </a>
+      <a href="instagram.com">
+        <img src="static/instagram48.png" alt="Instagram" />
+      </a>
+    </section>
+    <section className="contact-container">
+      <h5 className="phone">Phone: 08771 746 863</h5>
+      <h5 className="email">francis. whitehead@googlemail.com</h5>
+    </section>
     <style jsx>{`
-      .footer {
-        width: 100%;
-        background-color: #bfa3bf;
-        height: 80px;
-        display: flex;
-        font-family: var(--main-font);
+    .footer {
+      width: 100%;
+      background-color: #bfa3bf;
+      display: flex;
+      font-family: var(--main-font);
+    }
+    .contact-container {
+      color: var(--main-text-colour);
+      font-size: 1rem;
+    }
+    .contact-container, .icon-list {
+      // border: solid red 2px;
+      margin: auto;
+    }
+    .icon-list a {
+      text-decoration: none;
+      transition: background-color 0.25s;
+      margin: var(--header-footer-margin);
+      display: inline-block;
       }
-      .footer a {
-        color: white;
-        padding: 10px 30px;
-        text-decoration: none;
-        text-align: center;
-
-        transition: background-color 0.25s;
-        margin: var(--header-footer-margin);
-      }
-      .footer a:hover {
-        background: var(--link-hover-bck-main);
-      }
-      .phone {
-        color: var(--main-text-colour);
-        font-size: 1rem;
-      }
+    .icon-list a:hover {
+      background: var(--link-hover-bck-main);
+    }
+    .icon-list a img {
+      height: 30px;
+    }
     `}</style>
   </footer>
 );
